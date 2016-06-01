eecbac8f5706ac46beb56ebc22dd89c98e1d820e
vtoanb <vtoanb@gmail.com>
2016-06-01 10:29:20 +0700

    Add function main to me.c

--- me.c
+++ me.c
@@ -1,1 +1,4 @@
 /bin /cmd /dev /etc /git-bash.exe /git-cmd.exe /LICENSE.txt /mingw64 /proc /ReleaseNotes.html /tmp /unins000.dat /unins000.exe /unins000.msg /usr Update me.c from local file */
+int main(){
+	printf("Hello World from local machine");
+}
